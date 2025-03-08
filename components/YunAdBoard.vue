1a86db83fc86c6f3d86c1fef482ce7ab2f553d99
Acidbarium <acidbarium@163.com>
2025-03-08 18:20:53 +0800

    倒退一个我觉得我能把控的版本

---
+++ components/YunAdBoard.vue
@@ -0,0 +1,33 @@
+<template>
+  <YunCard>
+    <a
+      href="mailto:me@yunyoujun.cn"
+      target="_blank"
+      class="ad-link"
+    >
+      <img src="https://acidbarium.github.io/img/acidbarium3.png" alt="广告" class="ad-image" />
+
+    </a>
+  </YunCard>
+</template>
+
+<style scoped>
+.ad-link {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+  color: inherit;
+}
+
+.ad-image {
+  width: 100%;
+  height: auto;
+  /* 根据需要调整图片的宽高 */
+}
+
+.ad-text {
+  margin-top: 0px;
+  /* 根据需要调整文字与图片之间的间距 */
+}
+</style>
