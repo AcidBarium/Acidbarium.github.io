e3421656bd6cb16c02323246c7ff6a161cbe3cf7
Acidbarium <acidbarium@163.com>
2025-03-08 14:39:08 +0800

    尝试增加侧面选框

---
+++ components/YunAdBoard.vue
@@ -0,0 +1,12 @@
+<template>
+  <YunCard>
+    <a
+      href="/girls/"
+      target="_blank"
+      class="inline-flex-center gap-1"
+    >
+      <div class="i-ri-advertisement-line" />
+      <span>漂亮的女孩</span>
+    </a>
+  </YunCard>
+</template>
