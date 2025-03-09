3b514383f1ffe6bfaad643a809b581d1c3de03a0
Acidbarium <acidbarium@163.com>
2025-03-09 11:16:48 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -67,7 +67,7 @@ export default defineSiteConfig({
     {
       name: '贴吧',
       link: 'https://tieba.baidu.com/home/main?id=tb.1.56a8f3b9.yMsRJa_limLfEhbFCimkHQ?t=1727221816&fr=index',
-      icon: 'i-arcticons:baidu-tieba',
+      icon: 'i-simple-icons:baidu',
       color: '#0084FF',
     },
     // {
