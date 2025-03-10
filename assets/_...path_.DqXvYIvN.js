371650603b0ffa914da05f128a53cea0f8c8856a
AcidBarium <AcidBarium@users.noreply.github.com>
2025-03-10 10:08:40 +0000

    deploy: 036af10744ff886d3e200099845458c3dbf5920d

---
+++ assets/_...path_.DqXvYIvN.js
@@ -0,0 +1,1 @@
+import{a,u as s,e as t}from"./chunks/vue-router.Dq93Fous.js";import{j as n,K as r,P as c,O as p}from"./framework.DKdUf2Ot.js";const u=n({__name:"[...path]",setup(f){const o=s(),e=a();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,m)=>(p(),c("div"))}});typeof t=="function"&&t(u);export{u as default};
