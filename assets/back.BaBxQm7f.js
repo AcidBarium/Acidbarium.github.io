0540ff8eaed73e3ba000b2563f1a27e98b8b697e
AcidBarium <AcidBarium@users.noreply.github.com>
2025-04-18 00:48:02 +0000

    deploy: ce7c271f0bd454a1a9872a5054ccd60b37a38ad6

---
+++ assets/back.BaBxQm7f.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.O7rtq_z9.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
