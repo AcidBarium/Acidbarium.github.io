2ff3b5eb22538531824506357cca7eb66b679814
AcidBarium <AcidBarium@users.noreply.github.com>
2025-07-23 02:20:39 +0000

    deploy: d5607a5cf35cb31bdf6df220fd35fa7c7b919619

---
+++ assets/index.f8C5Xdj_.js
@@ -0,0 +1,50 @@
+import{_ as d}from"./ValaxyMain.vue_vue_type_style_index_0_lang.rfAWW-97.js";import{f as p,a as h}from"./chunks/vue-router.DhXd9BeV.js";import{A as c,N as g,a3 as e,U as o,R as u,u as b,O as f}from"./framework.jonq5HYD.js";import"./app.DUIVtafZ.js";import"./chunks/dayjs.CCYrSalk.js";import"./chunks/vue-i18n.BGLTdof4.js";import"./chunks/pinia.CUHRc3dr.js";import"./chunks/@vueuse/motion.Bq4lUiJd.js";import"./chunks/nprogress.BahbDzmd.js";import"./YunComment.vue_vue_type_style_index_0_lang.r1I2gomD.js";import"./index.C5okkQwF.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang.hhrGy7dI.js";import"./post.C5M7L3xo.js";const j=p("/projects",async a=>JSON.parse('{"title":"Acidbarium的小项目","description":"","frontmatter":{"title":"Acidbarium的小项目","tags":["项目"],"categories":["Acidbarium的小项目"],"date":"2023-09-01 00:00:00","updated":"2025-03-09 00:00:00","layout":"projects","projects":{"interesting":{"title":"娱乐","emoji":"🥰","collection":[{"name":"Eastern_Famous_School_Fantasyland","emoji":"🚀","color":"#4ea5f5","desc":"基于vsc++和easyx开发的类东方stg弹幕射击游戏","textColor":"white","github":"AcidBarium/Eastern_Famous_School_Fantasyland","url":"https://github.com/AcidBarium/Eastern_Famous_School_Fantasyland/releases"}]},"tools":{"title":"工具","emoji":"🔧","collection":[{"name":"GeneCardsWebScraper","emoji":"🧬","desc":"自动从genecards爬出相关基因的相关信息，并且提供多种翻译方式","color":"#f1e05a","github":"AcidBarium/GeneCardsWebScraper","url":"https://github.com/AcidBarium/GeneCardsWebScraper/releases","docs":"https://www.cnblogs.com/acidbarium/p/18731876"},{"name":"SSDUT-KeepMoving","emoji":"🏃🏻‍♂️‍➡️","desc":"大连理工大学软件学院keep打卡截图小工具(╹ڡ╹ )","color":"#6A5ACD","github":"AcidBarium/SSDUT-KeepMoving","url":"https://acidbarium.github.io/SSDUTkeepMoving/","docs":"https://acidbarium.github.io/posts/keepMoving"}]},"design":{"title":"设计","emoji":"🎨","collection":[{"name":"Codeforces_webpage_imitation","desc":"仿照codeforces官网制作的html和相应的css","color":"#0078E7","github":"AcidBarium/Cognitive_training_Codeforces_webpage_imitation","emoji":"🎈"}]},"open_source":{"title":"参与（暴论）","emoji":"👥","collection":[{"name":"DLUT_SE_Courses","emoji":"📖","desc":"大工开区的资料（之前往里面扔过一些东西）现在好像迁到大工云盘了。","github":"NAOSI-DLUT/DLUT_SE_Courses/tree/master","url":"http://pan.dlut.edu.cn/disk#groups%2Fjoined%2FNAOSI%2Ffiles","color":"#409eff","textColor":"white"},{"name":"DUTManual","emoji":"🏠","desc":"大工人的一站式生存指南","github":"NAOSI-DLUT/dut-manual","url":"https://man.naosi.org/","color":"black"},{"name":"blogroll","emoji":"🤝","desc":"收集大工人们的博客友链","github":"NAOSI-DLUT/blogroll","url":"https://blogroll.naosi.org/","color":"pink"}]},"other":{"title":"其他","emoji":"📁","collection":[{"name":"Blog","emoji":"🔗","desc":"我的另一处小博客","url":"https://www.cnblogs.com/acidbarium","github":"AcidBarium/Acidbarium.github.io"},{"name":"Acidbarium.github.io","emoji":"🔖","desc":"我的小网站","url":"https://acidbarium.github.io/","github":"AcidBarium/Acidbarium.github.io"}]}}},"headers":[],"relativePath":"pages/projects/index.md","lastUpdated":1753237162000}'),{lazy:(a,n)=>a.name===n.name}),G={__name:"index",setup(a,{expose:n}){var m;const{data:s}=j(),r=h(),i=Object.assign(r.meta.frontmatter||{},((m=s.value)==null?void 0:m.frontmatter)||{});return r.meta.frontmatter=i,c("pageData",s.value),c("valaxy:frontmatter",i),globalThis.$frontmatter=i,n({frontmatter:{title:"Acidbarium的小项目",tags:["项目"],categories:["Acidbarium的小项目"],date:"2023-09-01 00:00:00",updated:"2025-03-09 00:00:00",layout:"projects",projects:{interesting:{title:"娱乐",emoji:"🥰",collection:[{name:"Eastern_Famous_School_Fantasyland",emoji:"🚀",color:"#4ea5f5",desc:"基于vsc++和easyx开发的类东方stg弹幕射击游戏",textColor:"white",github:"AcidBarium/Eastern_Famous_School_Fantasyland",url:"https://github.com/AcidBarium/Eastern_Famous_School_Fantasyland/releases"}]},tools:{title:"工具",emoji:"🔧",collection:[{name:"GeneCardsWebScraper",emoji:"🧬",desc:"自动从genecards爬出相关基因的相关信息，并且提供多种翻译方式",color:"#f1e05a",github:"AcidBarium/GeneCardsWebScraper",url:"https://github.com/AcidBarium/GeneCardsWebScraper/releases",docs:"https://www.cnblogs.com/acidbarium/p/18731876"},{name:"SSDUT-KeepMoving",emoji:"🏃🏻‍♂️‍➡️",desc:"大连理工大学软件学院keep打卡截图小工具(╹ڡ╹ )",color:"#6A5ACD",github:"AcidBarium/SSDUT-KeepMoving",url:"https://acidbarium.github.io/SSDUTkeepMoving/",docs:"https://acidbarium.github.io/posts/keepMoving"}]},design:{title:"设计",emoji:"🎨",collection:[{name:"Codeforces_webpage_imitation",desc:"仿照codeforces官网制作的html和相应的css",color:"#0078E7",github:"AcidBarium/Cognitive_training_Codeforces_webpage_imitation",emoji:"🎈"}]},open_source:{title:"参与（暴论）",emoji:"👥",collection:[{name:"DLUT_SE_Courses",emoji:"📖",desc:"大工开区的资料（之前往里面扔过一些东西）现在好像迁到大工云盘了。",github:"NAOSI-DLUT/DLUT_SE_Courses/tree/master",url:"http://pan.dlut.edu.cn/disk#groups%2Fjoined%2FNAOSI%2Ffiles",color:"#409eff",textColor:"white"},{name:"DUTManual",emoji:"🏠",desc:"大工人的一站式生存指南",github:"NAOSI-DLUT/dut-manual",url:"https://man.naosi.org/",color:"black"},{name:"blogroll",emoji:"🤝",desc:"收集大工人们的博客友链",github:"NAOSI-DLUT/blogroll",url:"https://blogroll.naosi.org/",color:"pink"}]},other:{title:"其他",emoji:"📁",collection:[{name:"Blog",emoji:"🔗",desc:"我的另一处小博客",url:"https://www.cnblogs.com/acidbarium",github:"AcidBarium/Acidbarium.github.io"},{name:"Acidbarium.github.io",emoji:"🔖",desc:"我的小网站",url:"https://acidbarium.github.io/",github:"AcidBarium/Acidbarium.github.io"}]}}}}),(t,A)=>{const l=d;return f(),g(l,{frontmatter:b(i)},{"main-content-md":e(()=>[u(" more "),u(` #### AIS.js
+
+Tags: \`Vue\` / \`npm\` / \`Element\` / \`JavaScript\`
+
+> 船舶自动识别系统 (Automatic Identification System) 使用 js 构建 web 端。
+
+实现了 AIS 船舶报文（包括船位报告与安全相关确认信息）的串口读取与解析。
+
+解析包打包为 npm 包 [ais-json](https://www.npmjs.com/package/ais-json)
+
+- Web: <https://ais.js.org>
+- GitHub: [ais.js](https://github.com/YunYouJun/ais.js)
+- GitHub: [ais-json](https://github.com/YunYouJun/ais-json)
+
+#### Element-Theme-Ink
+
+Tags: \`Element\` / \`Scss\` / \`Theme\`
+
+> Dead simple css theme about element.
+
+基于 [Element](https://github.com/ElemeFE/element) 所写的极简主题样式。
+
+- Web: <https://ink.yunyoujun.cn/>
+- GitHub: [element-theme-ink](https://github.com/YunYouJun/element-theme-ink)
+- GitHub: [element-theme-ink-preview](https://github.com/YunYouJun/element-theme-ink-preview)
+- npm: [element-theme-ink](https://www.npmjs.com/package/element-theme-ink)
+- unpkg: [element-theme-ink](https://unpkg.com/element-theme-ink)
+
+#### Paper Star
+
+> 跨平台飞行射击游戏
+
+- GitHub: <https://github.com/PaperStar>
+
+待补充...
+
+### Just for fun
+
+#### give-me-money
+
+Tags: \`Vue\` / \`Parcel\` / \`LeanCloud\`
+
+我很可爱，请给我钱。
+
+- Web: [预览地址](https://yunyoujun.github.io/give-me-money/)
+- GitHub: [give-me-money](https://github.com/YunYouJun/give-me-money)
+
+#### 仿 2048 小游戏
+
+待补充... `)]),"main-header":e(()=>[o(t.$slots,"main-header")]),"main-header-after":e(()=>[o(t.$slots,"main-header-after")]),"main-nav":e(()=>[o(t.$slots,"main-nav")]),"main-content-before":e(()=>[o(t.$slots,"main-content-before")]),"main-content":e(()=>[o(t.$slots,"main-content")]),"main-content-after":e(()=>[o(t.$slots,"main-content-after")]),"main-nav-before":e(()=>[o(t.$slots,"main-nav-before")]),"main-nav-after":e(()=>[o(t.$slots,"main-nav-after")]),comment:e(()=>[o(t.$slots,"comment")]),footer:e(()=>[o(t.$slots,"footer")]),aside:e(()=>[o(t.$slots,"aside")]),"aside-custom":e(()=>[o(t.$slots,"aside-custom")]),default:e(()=>[o(t.$slots,"default")]),_:3},8,["frontmatter"])}}};export{G as default,j as usePageData};
